e2802fa24da5fa3e9deba79d32754f558d63b70b
Prince Darko <padarko99@gamil.com>
2022-01-31 08:46:07 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'F-EHealth',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
